39fddd0afa5068c1c62e1da989dddfe012c2aa58
Emmanuel <emmsom506@gmail.com>
2026-03-13 15:48:36 +0000

    Chore home page

--- src/components/pages/home/home-page.tsx
+++ src/components/pages/home/home-page.tsx
@@ -1,16 +1,20 @@
-import {SmoothScroll} from "@/components/layout/smooth-scroll";
-import {HeroSection} from "@/features/home/hero-section";
+import { SmoothScroll } from "@/components/layout/smooth-scroll";
+import { HeroSection } from "@/features/home/sections/hero-section";
+import { HdmExperienceSection } from "@/features/home/sections/hdm-experience-section";
+import { StudentsSection } from "@/features/home/sections/students-section";
+import { StudentExperienceSection } from "@/features/home/sections/student-experience-section";
 
-
-export function HomePage ()  {
-  return(
-    <main className="flex items-start justify-start w-full min-h-screen bg-background text-foreground font-sans">
-      <SmoothScroll>
-        <div className="flex w-full flex-col bg-background">
-         <HeroSection />
-
-        </div>
-      </SmoothScroll>
-    </main>
-  )
+export function HomePage() {
+    return (
+        <main className="flex items-start justify-start w-full min-h-screen bg-background text-foreground font-sans">
+            <SmoothScroll>
+                <div className="flex w-full flex-col bg-background">
+                    <HeroSection />
+                    <HdmExperienceSection />
+                    <StudentsSection />
+                    <StudentExperienceSection />
+                </div>
+            </SmoothScroll>
+        </main>
+    )
 }
