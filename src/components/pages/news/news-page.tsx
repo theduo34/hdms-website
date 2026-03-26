a4a7ea876b37e78cfee881c2eb94bbe215587bf1
Emmanuel <emmsom506@gmail.com>
2026-03-26 23:06:20 +0000

    Chore new page

--- src/components/pages/news/news-page.tsx
+++ src/components/pages/news/news-page.tsx
@@ -1,8 +1,11 @@
+"use client";
+
+import NewsClient from "@/features/news/news-client";
 
 export function NewsPage() {
-  return(
-    <div>
-      News Page
-    </div>
-  )
+    return (
+        <main className="flex flex-col w-full min-h-screen">
+            <NewsClient />
+        </main>
+    );
 }
