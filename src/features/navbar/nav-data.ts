dbed961e5677047552d1d0b075eda33b5abfe8c6
Emmanuel <emmsom506@gmail.com>
2026-03-29 13:26:13 +0000

    Chore nav data contants

--- src/features/navbar/nav-data.ts
+++ src/features/navbar/nav-data.ts
@@ -10,11 +10,15 @@ export type NavItem = {
 export const topNavItems: NavItem[] = [
   { title: "News & Announcements", href: "/news-&-announcements" },
   { title: "Gallery", href: "/gallery" },
-  { title: "Events", href: "/events" },
+  { title: "Events", href: "/calender?filter=event" },
   { title: "Calender", href: "/calender"}
 ]
 
 export const navItems: NavItem[] = [
+  {
+    title: 'Home',
+    href: '/',
+  },
   {
     title: 'About',
     href: '/about-us',
@@ -29,10 +33,11 @@ export const navItems: NavItem[] = [
   },
   {
     title: "Programmes",
-    children: [
-      { title: "Curriculum", href: "/curriculum" },
-      { title: "Departments", href: "/departments" },
-    ],
+    href: '/programmes',
+    // children: [
+    //   { title: "Curriculum", href: "/curriculum" },
+    //   { title: "Departments", href: "/departments" },
+    // ],
   },
   {
     title: "Campus Life",
