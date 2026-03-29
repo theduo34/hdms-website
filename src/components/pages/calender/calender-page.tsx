cae1c63a6fa537fa3ea4cfa8dc92b7f462149694
Emmanuel <emmsom506@gmail.com>
2026-03-29 13:27:24 +0000

    Chore calender page

--- src/components/pages/calender/calender-page.tsx
+++ src/components/pages/calender/calender-page.tsx
@@ -1,11 +1,15 @@
-import { SmoothScroll } from "@/components/layout/smooth-scroll";
+import { Suspense } from "react"
+import { SmoothScroll } from "@/components/layout/smooth-scroll"
+import CalenderClient from "@/features/calender/calender-client"
 
 export function CalenderPage() {
     return (
-        <main className="flex items-start justify-start w-full min-h-screen bg-background text-foreground font-sans">
+        <main className="flex items-start justify-start w-full min-h-screen bg-background font-sans">
             <SmoothScroll>
                 <div className="flex w-full flex-col bg-background">
-                    {/* Calender Sections */}
+                    <Suspense>
+                        <CalenderClient />
+                    </Suspense>
                 </div>
             </SmoothScroll>
         </main>
